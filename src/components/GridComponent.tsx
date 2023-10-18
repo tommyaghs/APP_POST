af6ce1ea1fc5ec16525dff2a3d486eb97b6ad137
tommyaghs <tommyaghs@gmail.com>
2023-10-18 10:12:46 +0200

    Update GridComponent.tsx

--- src/components/GridComponent.tsx
+++ src/components/GridComponent.tsx
@@ -7,7 +7,7 @@ import { PropagateLoader } from 'react-spinners';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import CardComponent from './CardComponent';
-import PaginationComponent from './PaginationComponent'; 
+import PaginationComponent from './PaginationComponent';
 
 function GridComponent() {
   const dispatch: AppDispatch = useDispatch();
@@ -39,40 +39,39 @@ function GridComponent() {
   };
 
   return (
-<div className='mt-5'>
-  <div className='container'>
-    <div className='row justify-content-end mt-5'>
+    <div className='mt-5'>
+      <div className='container'>
+        <div className='row justify-content-end mt-5'>
           <h2 className='text-center'>Gli ultimi Post</h2>
-      <div className='col-lg-2'>
-        <select className='mb-4 form-select w-100 custom-dropdown' value={itemsPerPage} onChange={handleChangeItemsPerPage}>
-          <option value={5}>5 elementi</option>
-          <option value={10}>10 elementi</option>
-          <option value={20}>20 elementi</option>
-        </select>
+          <div className='col-lg-2'>
+            <select className='mb-4 form-select w-100 custom-dropdown' value={itemsPerPage} onChange={handleChangeItemsPerPage}>
+              <option value={5}>5 elementi</option>
+              <option value={10}>10 elementi</option>
+              <option value={20}>20 elementi</option>
+            </select>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  
       {
-    status === 'loading' ? (
-      <PropagateLoader className='text-center' color="rgba(0, 0, 0, 1)" />
-    ) : status === 'failed' ? (
-      <p>Error: {error}</p>
-    ) : (
-      <Row xs={1} md={5} className="g-4 ms-2 me-2">
-        {postsToShow.map((post: Post) => (
-          <Col key={post.id}>
-            <CardComponent post={post} />
-          </Col>
-        ))}
-      </Row>
-    )
-  }
-  <PaginationComponent
-    currentPage={currentPage}
-    totalPages={totalPages}
-    handlePageChange={handlePageChange}
-  />
+        status === 'loading' ? (
+          <PropagateLoader className='text-center' color="rgba(0, 0, 0, 1)" />
+        ) : status === 'failed' ? (
+          <p>Error: {error}</p>
+        ) : (
+          <Row xs={1} md={5} className="g-4 ms-2 me-2">
+            {postsToShow.map((post: Post) => (
+              <Col key={post.id}>
+                <CardComponent post={post} />
+              </Col>
+            ))}
+          </Row>
+        )
+      }
+      <PaginationComponent
+        currentPage={currentPage}
+        totalPages={totalPages}
+        handlePageChange={handlePageChange}
+      />
     </div >
   );
 }
